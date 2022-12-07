daec1ee75d0180a2235676fd32e7f72db4baf25a
logeast <logeast@outlook.com>
2022-12-07 11:43:51 +0800

    chore: update publicPath

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "dumi";
 export default defineConfig({
   mfsu: false,
   base: "/dumi-theme-vite/",
+  publicPath: "/dumi-theme-vite/",
   themeConfig: {
     name: "Copyboards",
   },
