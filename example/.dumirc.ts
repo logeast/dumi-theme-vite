015fc8d3e248d0df85d3cd8ad666a3181309c1c7
logeast <logeast@outlook.com>
2022-12-09 09:54:33 +0800

    chore: update packages

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   themeConfig: {
     name: "Dumpress",
   },
+  // locales: [
+  //   { id: 'en-US', name: 'English', base: '/' },
+  //   { id: 'zh-CN', name: '中文', base: '/cn' },
+  // ]
 });
