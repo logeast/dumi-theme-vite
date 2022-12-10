816424cdd148e537fb25371c3f4dcbc38d29023b
logeast <logeast@outlook.com>
2022-12-10 19:03:49 +0800

    feat: update tailwindcss

---
+++ .umirc.ts
@@ -0,0 +1,1 @@
+export default { plugins: ["@umijs/plugins/dist/tailwindcss"] };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+  content: [
+    './src/layouts/**.tsx',
+  ],
+}
