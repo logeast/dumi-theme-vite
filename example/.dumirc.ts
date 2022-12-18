c7ca88ea46682eecfd15708a55d02af09dc0687f
logeast <logeast@outlook.com>
2022-12-18 14:13:50 +0800

    fix: remove locales

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   themeConfig: {
     name: "Dumpress",
   },
-  locales: [
-    { id: "en-US", name: "English" },
-    { id: "zh-CN", name: "中文" },
-  ],
+  // locales: [
+  //   { id: "en-US", name: "English" },
+  //   { id: "zh-CN", name: "中文" },
+  // ],
 });
